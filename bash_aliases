e37ad106a6d4275ff817ebe672d8855731c15e3a
Simon Atanasyan <simon@atanasyan.com>
2013-05-05 20:18:45 +0400

    Initial commit.

---
+++ bash_aliases
@@ -0,0 +1,9 @@
+# some ls aliases
+alias ll='ls -l'
+alias la='ls -A'
+
+# path aliases
+alias ..='cd ..'
+
+alias m='less'
+alias ps='ps fax'
---
+++ bash_completion.d/bld_scripts.sh
@@ -0,0 +1,54 @@
+_clang_cfg() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    
+    opts=" \
+    debug \
+    release \
+    wignore"
+
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+    return 0
+}
+
+_clang_bld() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    
+    opts=" \
+    verbose \
+    install \
+    check"
+
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+    return 0
+}
+
+_mc_llvm() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    
+    opts=" \
+    debug \
+    release"
+
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+    return 0
+}
+
+complete -F _clang_cfg clang-cfg.sh
+complete -F _clang_bld clang-bld.sh
+complete -F _clang_cfg lld-cfg.sh
+complete -F _clang_bld lld-bld.sh
+complete -F _mc_llvm   mc-llvm.sh
+complete -F _mc_llvm   mc-cfg.sh
+complete -F _clang_bld mc-bld.sh
